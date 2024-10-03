7c1c40c0fe44a642be6f6086daec362372f6aa24
Jossani da Cruz <jossanidacruz2@gmail.com>
2024-10-03 22:33:53 +0100

    prevent eslint erros from log in build process

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,12 @@ const nextConfig = {
       protocol: 'https',
       hostname: 'lh3.googleusercontent.com'
     }]
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
